2484caef181b71ca4c446605c308f14087176cdb
eulseon <95990910+es915@users.noreply.github.com>
2022-02-14 18:51:26 +0900

    Configure the multi-directionality of the object

---
+++ Versatility-of-objects.java
@@ -0,0 +1,62 @@
+class Main {
+  public static void main(String[] args) {
+   
+    
+    Weapon w1 = new Sword();
+    w1.attack();
+    // 출력 : 칼로 공격합니다.
+
+    w1 = new Bow();
+    w1.attack();
+    // 출력 : 활로 공격합니다.
+    
+    System.out.println();
+    
+    Warrior warrior = new Warrior(); 
+
+    warrior.age = 20;
+    warrior.name = "홍길동";
+
+    warrior.weapon = new Bow();
+    warrior.attack();    
+    // 홍길동이 활로 공격합니다.
+    
+    warrior.weapon = new Sword();
+    warrior.attack();
+    // 홍길동이 칼로 공격합니다.
+  }
+}
+
+class Weapon {
+	void attack() {
+		System.out.println("무기로 공격합니다");
+	}
+}
+
+class Sword extends Weapon {
+	void attack() {
+		System.out.println("칼로 공격합니다");
+	}
+}
+
+class Bow extends Weapon {
+	void attack() {
+		System.out.println("활로 공격합니다");
+	}
+}
+
+class Warrior {
+	int age;
+	String name;
+	Weapon weapon;
+	
+	void attack() {
+		System.out.print("홍길동이 ");
+		weapon.attack();
+	}
+	
+	void chageWeapon(Weapon weapon) {
+		this.weapon = weapon;
+	}
+	
+}
