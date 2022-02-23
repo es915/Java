468bfaf985aebee5b5410e0c92cd0fa0549247cb
eulseon <95990910+es915@users.noreply.github.com>
2022-02-23 23:37:37 +0900

    Implement search function

---
+++ post/Article.java
@@ -0,0 +1,42 @@
+lass Article {
+	
+	private int idx;
+	private String title;
+	private String body;
+	
+	// 기본 생성자
+	public Article() {}
+	
+	// 모든 매개변수 받는 생성자
+	public Article(int idx, String title, String body) {
+		super();
+		this.idx = idx;
+		this.title = title;
+		this.body = body;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+}
